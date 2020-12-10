e094c2ebc2d80f88cbf73babde35ca0501d1cfc7
mondayoffkolin <72369518+mondayoffkolin@users.noreply.github.com>
2020-12-10 17:57:56 +0900

    #1.1 전체적인 틀 구현중..
    # 시도해볼 것들
    1. 엘리베이터): 타이쿤 형식 (Pinterest View)
    2. 엘리베이터): Lion Studio View
    3. 책): 일기장 형식

---
+++ ProjectFrame/Assets/02Scripts/InGame1/Manager/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] LayerMask m_doorlayerMask;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 a_mousePos = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(a_mousePos);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity, m_doorlayerMask))
+            {
+                print("Door hit");
+
+                GameObject a_obj = hit.transform.gameObject;
+                a_obj.GetComponent<DoorProduction>().DoorOpen();
+            }
+        }
+    }
+}
---
+++ ProjectFrame/Assets/02Scripts/InGame1/Production/DoorProduction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoorProduction : MonoBehaviour
+{
+    [Header("문 연출 관련")]
+    [SerializeField] bool m_isOpen = false;
+    [SerializeField] Transform m_openTf = null;
+    [SerializeField] Transform m_closeTf = null;
+
+    Sequence doorSeq;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    public void DoorOpen()
+    {
+        if(m_isOpen == false)
+        {
+            m_isOpen = true;
+
+            doorSeq = DOTween.Sequence()
+                            .Append(this.transform.DORotate(m_openTf.eulerAngles, 2f));
+        }
+    }
+}
